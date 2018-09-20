35f52e302c271f197117bf20225e23ad645cbf0a
Mattia Salvini <mattiasalvini@Mattias-MacBook-Pro.local>
2018-09-20 10:04:55 +0200

    More clear plots with legend

--- envs/executeEnv.py
+++ envs/executeEnv.py
@@ -7,6 +7,8 @@ import plotting as plot
 env = gym.make('LQG1D-v0')
 eps = 10**-16
 
+
+
 def reinforce(env, num_episodes, batch_size, discount_factor=1.0):
     """
     REINFORCE (Monte Carlo Policy Gradient) Algorithm. Optimizes the policy
@@ -23,39 +25,64 @@ def reinforce(env, num_episodes, batch_size, discount_factor=1.0):
     """
     param = -np.ones(np.shape(env.state))
     # Keeps track of useful statistics#
+    stats_opt = plot.EpisodeStats(
+        episode_lengths=np.zeros(num_episodes),
+        episode_rewards=np.zeros(num_episodes))
+
     stats = plot.EpisodeStats(
         episode_lengths=np.zeros(num_episodes),
         episode_rewards=np.zeros(num_episodes))
 
     Transition = collections.namedtuple("Transition", ["state", "action", "reward", "next_state", "done"])
     Episode_values = collections.namedtuple("Episode_values", ["gradient_estimate", "total_reward"])
+
     # Iterate for all batch
     num_batch = num_episodes//batch_size
     for i_batch in range(num_batch):
+
         # Iterate for every episode in batch
         for i_episode in range(batch_size):
-            # Reset the environment and pick the fisrst action
+            # Reset the environment and pick the first action
             state = env.reset()
             episode_informations = []
             episode = []
             total_return = 0
             gradient_est = 0
+
+            K = env.computeOptimalK()
+
             # One step in the environment
             for t in range(100):
-
-                env.render()
+                #env.render()
                 # Take a step
                 mean = param*state
                 variance = 1
                 action = np.random.normal(mean, variance)
                 next_state, reward, done, _ = env.step(action)
                 # Keep track of the transition
-                episode.append(Transition(
-                  state=state, action=action, reward=reward, next_state=next_state, done=done))
+                episode.append(Transition(state=state, action=action, reward=reward, next_state=next_state, done=done))
 
                 # Update statistics
                 stats.episode_rewards[i_episode] += reward
                 stats.episode_lengths[i_episode] = t
+
+                if done:
+                    break
+
+                state = next_state
+
+
+            state = env.reset()
+            #
+            for t in range(100):
+                #env.render()
+                # Take a step
+                action = K * state
+                next_state, reward, done, _ = env.step(action)
+
+                # Update statistics
+                stats_opt.episode_rewards[i_episode] += reward
+                stats_opt.episode_lengths[i_episode] = t
 
                 if done:
                     break
@@ -74,14 +101,12 @@ def reinforce(env, num_episodes, batch_size, discount_factor=1.0):
         if abs(par_old-param) <= eps:
             break
 
-    return stats
+    return stats, stats_opt
 
-stats = reinforce(env, 100, 10, discount_factor=0.6)
-plot.plot_episode_stats(stats, smoothing_window=25)
-# For plotting: Create value function from action-value function
-# by picking the best action at each state
-# V = defaultdict(float)
-# for state, actions in Q.items():
-    # action_value = np.max(actions)
-    # V[state] = action_value
-# plotting.plot_value_function(V, title="Optimal Value Function")
+
+
+stats, stats_opt = reinforce(env, 100, 10, discount_factor=0.6)
+
+#plot the statistics of the algorithm
+plot.plot_episode_stats(stats, stats_opt, 100, smoothing_window=1)
+#plot.plot_episode_stats(stats_opt, smoothing_window=1)
