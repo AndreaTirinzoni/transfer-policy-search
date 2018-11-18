8fff058afad2e6037fb1593975ae60fa13a5e7eb
MattiaSalvini <mattia.salvini@mail.polimi.it>
2018-11-18 20:01:50 +0100

    Fixed problem with source task creation

--- algorithmsComparisonOffPolicyJoblib.py
+++ algorithmsComparisonOffPolicyJoblib.py
@@ -31,7 +31,7 @@ def simulation(env, batch_size, discount_factor, variance_action, episode_length
     """
 
     print("Creating source task")
-    [source_task, episodes_per_config, source_param, next_states_unclipped, actions_clipped] = stc.sourceTaskCreation(episode_length, episodes_per_configuration, discount_factor, variance_action, env_param_min, env_param_max, policy_param_min, policy_param_max)
+    [source_task, source_param, episodes_per_config, next_states_unclipped, actions_clipped] = stc.sourceTaskCreation(episode_length, episodes_per_configuration, discount_factor, variance_action, env_param_min, env_param_max, policy_param_min, policy_param_max)
 
 
     print("Learning policy")
@@ -113,7 +113,7 @@ policy_param_max = 0
 
 seeds = [np.random.randint(1000000) for _ in range(runs)]
 
-results = Parallel(n_jobs=5)(delayed(simulation)(env, batch_size, discount_factor, variance_action, episode_length, mean_initial_param, variance_initial_param, num_batch, seed, episodes_per_configuration, env_param_min, env_param_max, policy_param_min, policy_param_max) for seed in seeds)
+results = Parallel(n_jobs=1)(delayed(simulation)(env, batch_size, discount_factor, variance_action, episode_length, mean_initial_param, variance_initial_param, num_batch, seed, episodes_per_configuration, env_param_min, env_param_max, policy_param_min, policy_param_max) for seed in seeds)
 
 for i_run in range(runs):
 
