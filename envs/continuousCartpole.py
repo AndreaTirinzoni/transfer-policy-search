Code ok, missing simulation with cartpole, there is a bug in the weights computation (transitions?)

envs/continuousCartpole.py
@@ -62,10 +62,11 @@ class ContinuousCartPoleEnv(gym.Env):
         return [seed]
 
     def getEnvParam(self):
-        return [self.masscart, self.length, self.sigma_env**2]
+        return np.asarray([np.ravel(self.masscart), np.ravel(self.length), np.ravel(self.sigma_env**2)])
 
     def setParams(self, env_param):
-        [self.masscart, self.length] = env_param[0:1]
+        self.masscart = env_param[0]
+        self.length = env_param[1]
         self.sigma_env = np.sqrt(env_param[-1])
 
     def stepPhysics(self, force):
@@ -111,6 +112,55 @@ Any further steps are undefined behavior.
             reward = 0.0
 
         return [np.array(self.state), float(reward), done, np.array(self.state), u]
+
+    def stepDenoised(self, env_parameters, state, action):
+        force = self.force_mag * action
+        state_t1 = self.stepPhysicsDenoised(env_parameters, state, force)
+        return state_t1
+
+    def stepPhysicsDenoised(self, env_parameters, state, force):
+        x = state[:, :, 0, :]
+        x_dot = state[:, :, 1, :]
+        theta = state[:, :, 2, :]
+        theta_dot = state[:, :, 3, :]
+        costheta = np.cos(theta)
+        sintheta = np.sin(theta)
+        masscart = env_parameters[:, 0]
+        length = env_parameters[:, 1]
+        total_mass = (self.masspole * np.ones(masscart.shape[0]) + masscart)
+        polemass_length = (self.masspole * length)
+        temp = (np.repeat(force[:, :, :, np.newaxis], masscart.shape[0], axis=3) + polemass_length[np.newaxis, np.newaxis, np.newaxis, :] * theta_dot * theta_dot * sintheta) / total_mass[np.newaxis, np.newaxis, np.newaxis, :]
+        thetaacc = (self.gravity * sintheta - costheta * temp) / \
+            (length[np.newaxis, np.newaxis, np.newaxis, :] * (4.0/3.0 - self.masspole * costheta * costheta / total_mass[np.newaxis, np.newaxis, np.newaxis, :]))
+        xacc = temp - polemass_length * thetaacc * costheta / total_mass[np.newaxis, np.newaxis, np.newaxis, :]
+        x = x + self.tau * x_dot
+        x_dot = x_dot + self.tau * xacc
+        theta = theta + self.tau * theta_dot
+        theta_dot = theta_dot + self.tau * thetaacc
+        return (x, x_dot, theta, theta_dot)
+
+    def stepCurrentDenoised(self, state, action):
+        force = self.force_mag * action
+        state_t1 = self.stepPhysicsDenoised(state, force)
+        return state_t1
+
+    def stepPhysicsCurrentDenoised(self, state, force):
+        x = state[:, :, 0]
+        x_dot = state[:, :, 1]
+        theta = state[:, :, 2]
+        theta_dot = state[:, :, 3]
+        costheta = np.cos(theta)
+        sintheta = np.sin(theta)
+        temp = (force + self.polemass_length * theta_dot * theta_dot * sintheta) / self.total_mass
+        thetaacc = (self.gravity * sintheta - costheta * temp) / \
+            (self.length * (4.0/3.0 - self.masspole * costheta * costheta / self.total_mass))
+        xacc = temp - self.polemass_length * thetaacc * costheta / self.total_mass
+        x = x + self.tau * x_dot
+        x_dot = x_dot + self.tau * xacc
+        theta = theta + self.tau * theta_dot
+        theta_dot = theta_dot + self.tau * thetaacc
+
+        return (x, x_dot, theta, theta_dot)
 
     def reset(self):
         self.state = self.np_random.uniform(low=-0.05, high=0.05, size=(4,))

envs/lqg1d.py
@@ -54,8 +54,13 @@ class LQG1D(gym.Env):
         self.reset()
 
     def setParams(self, env_param):
-        [A, B] = env_param[0:2]
-        self.sigma_noise = env_param[-1]
+        dim1a = self.A.shape[0]
+        dim2a = self.A.shape[1]
+        self.A = env_param[0:(dim1a*dim2a)].reshape((dim1a, dim2a))
+        dim1b = self.B.shape[0]
+        dim2b = self.B.shape[1]
+        self.B = env_param[(dim1a*dim2a):(dim1a*dim2a)+(dim1b*dim2b)].reshape((dim1b, dim2b))
+        self.sigma_noise = m.sqrt(env_param[-1])
 
     def step(self, action, render=False):
         u = np.clip(action, -self.max_action, self.max_action)
@@ -72,20 +77,24 @@ class LQG1D(gym.Env):
     #Custom param for transfer
 
     def getEnvParam(self):
-        return [self.A, self.B, self.sigma_noise**2]
+        return np.asarray([np.ravel(self.A, order="C"), np.ravel(self.B, order="C"), np.ravel(self.sigma_noise**2, order="C")])
 
     def stepDenoised(self, env_parameters, state, action):
-        u = np.clip(action, -self.max_action, self.max_action)
-        A = env_parameters[0:self.param_dim][np.newaxis, :]
-        B = env_parameters[self.param_dim:self.param_dim+self.param_dim]
-        xn_unclipped = np.matmul(A, state, axis=2) + np.multiply(B, u)
-        xn = np.clip(xn_unclipped, -self.max_pos, self.max_pos)
-        cost = np.multiply(state, np.sum(np.multiply(self.Q[np.newaxis, :], state), axis=2)[:, :, np.newaxis]) + np.multiply(u, np.multiply(self.R, u))[:, :, np.newaxis]
+        num_episodes = env_parameters.shape[0]
+        dim1a = self.A.shape[0]
+        dim2a = self.A.shape[1]
+        A = env_parameters[:, 0:(dim1a*dim2a)].reshape((num_episodes, dim1a, dim2a))
+        dim1b = self.B.shape[0]
+        dim2b = self.B.shape[1]
+        B = env_parameters[:, (dim1a*dim2a):(dim1a*dim2a)+(dim1b*dim2b)].reshape((num_episodes, dim1b, dim2b))
+        xn_unclipped = np.multiply((A.T)[:, :, np.newaxis, :], state) + np.multiply(B.T, action)[:, :, np.newaxis, :]
+        return xn_unclipped
 
-        state = np.matrix(xn.ravel())
-
-        # We return the unclipped state and the clipped action as the last argument (to be used for computing the importance weights only)
-        return state, -np.asscalar(cost), False, np.array(xn_unclipped.ravel()), u
+    def stepDenoisedCurrent(self, state, action):
+        A = self.A
+        B = self.A
+        xn_unclipped = np.multiply((A.T)[np.newaxis, :, :], state) + np.multiply(B, action)[:, :, np.newaxis]
+        return xn_unclipped
 
     def reset(self, state=None):
         if state is None:

simulation.py
@@ -46,10 +46,10 @@ class SourceDataset:
         self.source_distributions = None
 
 
-env = gym.make('testing-v0')
-param_space_size = 2
-state_space_size = 2
-env_param_space_size = 2
+env = gym.make('LQG1D-v0')
+param_space_size = 1
+state_space_size = 1
+env_param_space_size = 3
 episode_length = 20
 
 env_param = EnvParam(env, param_space_size, state_space_size, env_param_space_size, episode_length)
@@ -88,11 +88,11 @@ policy_param_min = -1
 policy_param_max = -0.1
 linspace_env = 11
 linspace_policy = 10
-policy_params = np.matrix([[2, 2], [2, 2]])
-env_params = np.matrix([[2, 0.09], [1, 0.09]])
+# policy_params = np.matrix([[2, 2], [2, 2]])
+# env_params = np.matrix([[2, 0.09], [1, 0.09]])
 
-
-[source_task, source_param, episodes_per_config, next_states_unclipped, actions_clipped] = stc.sourceTaskCreationSpec(env, episode_length, batch_size, discount_factor, variance_action, policy_params, env_params, param_space_size, state_space_size, env_param_space_size)
+[source_task, source_param, episodes_per_config, next_states_unclipped, actions_clipped] = stc.sourceTaskCreationAllCombinations(env, episode_length, batch_size, discount_factor, variance_action, env_param_min, env_param_max, policy_param_min, policy_param_max, linspace_env, linspace_policy, param_space_size, state_space_size, env_param_space_size)
+# [source_task, source_param, episodes_per_config, next_states_unclipped, actions_clipped] = stc.sourceTaskCreationSpec(env, episode_length, batch_size, discount_factor, variance_action, policy_params, env_params, param_space_size, state_space_size, env_param_space_size)
 
 # source_task = np.genfromtxt('source_task.csv', delimiter=',')
 # episodes_per_config = np.genfromtxt('episodes_per_config.csv', delimiter=',').astype(int)
@@ -125,9 +125,9 @@ for i_run in range(runs):
     # estimator = "MIS-CV"
     # off_policy_mis_cv = la.learnPolicy(env_param, simulation_param, source_dataset, estimator, off_policy=1) #1e-6
     #
-    # print("MIS-CV-BASELINE")
-    # estimator = "MIS-CV-BASELINE"
-    # off_policy_mis_cv_baseline = la.learnPolicy(env_param, simulation_param, source_dataset, estimator, off_policy=1) #1e-6
+    print("MIS-CV-BASELINE")
+    estimator = "MIS-CV-BASELINE"
+    off_policy_mis_cv_baseline = la.learnPolicy(env_param, simulation_param, source_dataset, estimator, off_policy=1) #1e-6
 
     # print("PD-MIS")
     # estimator = "PD-MIS"
@@ -137,14 +137,22 @@ for i_run in range(runs):
     # estimator = "PD-MIS-CV"
     # off_policy_pd_mis_cv = la.learnPolicy(env_param, simulation_param, source_dataset, estimator) #1e-6
     #
-    print("PD-MIS-CV-BASELINE-APPROXIMATED")
-    estimator = "PD-MIS-CV-BASELINE-APPROXIMATED"
-    off_policy_pd_mis_cv_baseline_approx = la.learnPolicy(env_param, simulation_param, source_dataset, estimator) #1e-6
-    #
+    # print("PD-MIS-CV-BASELINE-APPROXIMATED")
+    # estimator = "PD-MIS-CV-BASELINE-APPROXIMATED"
+    # off_policy_pd_mis_cv_baseline_approx = la.learnPolicy(env_param, simulation_param, source_dataset, estimator) #1e-6
+    # #
     # print("PD-MIS-CV-BASELINE")
     # estimator = "PD-MIS-CV-BASELINE"
     # off_policy_pd_mis_cv_baseline = la.learnPolicy(env_param, simulation_param, source_dataset, estimator) #1e-6
 
-    print("REINFORCE")
-    estimator = "REINFORCE"
-    reinforce = la.learnPolicy(env_param, simulation_param, source_dataset, estimator) #1e-6
+    # print("REINFORCE")
+    # estimator = "REINFORCE"
+    # reinforce = la.learnPolicy(env_param, simulation_param, source_dataset, estimator, off_policy=0) #1e-6
+    #
+    # print("REINFORCE-BASELINE")
+    # estimator = "REINFORCE-BASELINE"
+    # reinforce_baseline = la.learnPolicy(env_param, simulation_param, source_dataset, estimator, off_policy=0) #1e-6
+
+    print("GPOMDP")
+    estimator = "GPOMDP"
+    gpomdp = la.learnPolicy(env_param, simulation_param, source_dataset, estimator, off_policy=0) #1e-6

sourceTaskCreation.py
@@ -24,7 +24,7 @@ def createBatch(env, batch_size, episode_length, param, state_space_size, varian
             # Take a step
             mean_action = np.sum(np.multiply(param, state))
             action = np.random.normal(mean_action, m.sqrt(variance_action))
-            next_state, reward, done, unclipped_state, clipped_action, state_denoised = env.step(action)
+            next_state, reward, done, unclipped_state, clipped_action = env.step(action)
             # Keep track of the transition
             #env.render()
             batch[i_batch, t, 0:state_space_size] = state
@@ -67,11 +67,10 @@ def sourceTaskCreationAllCombinations(env, episode_length, batch_size, discount_
     i_configuration = 0
     episode_per_param = batch_size
     length_source_task = policy_param.shape[0]*env_param.shape[0]*episode_per_param
-    source_task = np.zeros((length_source_task, episode_length*(state_space_size + 2) + state_space_size)) # every line a task, every task has all [clipped_state, action, reward]
+    source_task = np.zeros((length_source_task, episode_length, state_space_size + 2 + state_space_size)) # every line a task, every task has all [clipped_state, action, reward]
     # Every line is a task, every task has [discounted_return, policy_parameter, env_params, variance]
     source_param = np.zeros((length_source_task, 1+param_space_size+env_param_space_size))
     next_states_unclipped = np.zeros((length_source_task, episode_length, state_space_size))
-    next_states_denoised = np.zeros((length_source_task, episode_length, state_space_size))
     actions_clipped = np.zeros((length_source_task, episode_length))
 
     discount_factor_timestep = np.power(discount_factor*np.ones(episode_length), range(episode_length))
@@ -80,32 +79,29 @@ def sourceTaskCreationAllCombinations(env, episode_length, batch_size, discount_
 
         for i_env_param in range(env_param.shape[0]):
 
-            env.setParams(np.array([env_param[i_env_param], env.B, env.sigma_noise**2]))
+            env.setParams(np.concatenate(([env_param[i_env_param]], np.ravel(env.B), [env.sigma_noise**2])))
 
             # Reset the environment and pick the first action
             batch = createBatch(env, episode_per_param, episode_length, policy_param[i_policy_param], state_space_size, variance_action) # [state, action, reward, next_state]
 
             #  Go through the episode and compute estimators
-
+            #I populate the source task
             discounted_return = np.sum((discount_factor_timestep * batch[:, :, state_space_size+1]), axis=1)
 
             #I populate the source task
-            dim1 = batch[:, :, 0:state_space_size].shape[0]
-            dim2 = batch[:, :, 0:state_space_size].shape[1]
-            dim3 = batch[:, :, 0:state_space_size].shape[2]
-            source_task[i_episode:i_episode+episode_per_param, 0::(state_space_size + 2)] = np.concatenate((np.reshape(batch[:, :, 0:state_space_size], (dim1, dim2*dim3), order='C'), (batch[:, -1, 0:state_space_size])), axis=1)
-            source_task[i_episode:i_episode+episode_per_param, state_space_size::(state_space_size + 2)] = batch[:, :, -1]
-            source_task[i_episode:i_episode+episode_per_param, state_space_size+1::(state_space_size + 2)] = batch[:, :, state_space_size+1]
+            source_task[i_episode:i_episode+episode_per_param, :, 0:state_space_size] = batch[:, :, 0:state_space_size]
+            source_task[i_episode:i_episode+episode_per_param, :, state_space_size] = batch[:, :, state_space_size+2+state_space_size+state_space_size]
+            source_task[i_episode:i_episode+episode_per_param, :, state_space_size+1] = batch[:, :, state_space_size+1]
+            source_task[i_episode:i_episode+episode_per_param, :, state_space_size+2:] = batch[:, :, state_space_size+2:state_space_size+2+state_space_size]
 
             #unclipped next_states and actions
             next_states_unclipped[i_episode:i_episode+episode_per_param, :] = batch[:, :, state_space_size+2+state_space_size:state_space_size+2+state_space_size+state_space_size]
-            next_states_denoised[i_episode:i_episode+episode_per_param, :] = batch[:, :, -1]
             actions_clipped[i_episode:i_episode+episode_per_param, :] = batch[:, :, state_space_size]
 
             #I populate the source parameters
             source_param[i_episode:i_episode+episode_per_param, 0] = discounted_return
-            source_param[i_episode:i_episode+episode_per_param, 1:1+param_space_size] = policy_param[:, i_policy_param]
-            source_param[i_episode:i_episode+episode_per_param, 1+param_space_size:1+param_space_size+env_param_space_size] = env.getEnvParam()
+            source_param[i_episode:i_episode+episode_per_param, 1:1+param_space_size] = policy_param[i_policy_param]
+            source_param[i_episode:i_episode+episode_per_param, 1+param_space_size:1+param_space_size+env_param_space_size] = env.getEnvParam().T
 
             i_episode += episode_per_param
 
@@ -113,7 +109,7 @@ def sourceTaskCreationAllCombinations(env, episode_length, batch_size, discount_
             i_configuration += 1
 
 
-    return source_task, source_param, episodes_per_configuration.astype(int), next_states_unclipped, actions_clipped, next_states_denoised
+    return source_task, source_param, episodes_per_configuration.astype(int), next_states_unclipped, actions_clipped
 
 def sourceTaskCreationSpec(env, episode_length, batch_size, discount_factor, variance_action, policy_params, env_params, param_space_size, state_space_size, env_param_space_size):
     """