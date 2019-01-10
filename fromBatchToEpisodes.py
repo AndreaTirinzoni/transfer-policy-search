633ab0cf0be4d654926cef80d12d26d6f28b2ac1
MattiaSalvini <mattia.salvini@mail.polimi.it>
2019-01-10 12:47:49 +0100

    Adding code for plotting using number of episodes

--- fromBatchToEpisodes.py
+++ fromBatchToEpisodes.py
@@ -1,3 +1,15 @@
 import numpy as np
 
-def
+def batchToEpisodes(statistic_batch, episodesPerBatch, linspace_episodes):
+
+    tot_episodes = np.sum(episodesPerBatch)
+    statistic_episode = np.ones(tot_episodes)
+
+    initial_index = 0
+
+    for i in range(tot_episodes):
+        episode_current_batch = episodesPerBatch[i]
+        statistic_episode[initial_index:initial_index+episode_current_batch] = statistic_batch
+        initial_index += episode_current_batch
+
+    return statistic_episode[0::linspace_episodes]
