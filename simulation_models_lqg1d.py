0eae7282200977513c7741fcdb84c9ced2962472
MattiaSalvini <mattia.salvini@mail.polimi.it>
2019-01-10 09:33:50 +0100

    Modify simulation of lqg1d with model estimation

--- simulation_models_lqg1d.py
+++ simulation_models_lqg1d.py
@@ -1,12 +1,47 @@
 import gym
 import envs
 import numpy as np
+import algorithmPolicySearch as alg
 import learningAlgorithm as la
 import sourceTaskCreation as stc
 import pickle
 from utils import plot
 import math as m
+from joblib import Parallel
+from joblib import delayed
 import simulationClasses as sc
+
+def simulationParallel(env_src, episode_length, source_dataset_batch_size, discount_factor, variance_action, env_param_min, env_param_max, policy_param_min, policy_param_max, linspace_env, linspace_policy, param_space_size, state_space_size, env_param_space_size, estimators, learning_rates, env_param, simulation_param, seed): #lqg simulation
+
+    np.random.seed(seed)
+
+    [source_task, source_param, episodes_per_configuration, next_states_unclipped, actions_clipped, next_states_unclipped_denoised] = stc.sourceTaskCreationAllCombinations(env_src, episode_length, source_dataset_batch_size, discount_factor, variance_action, env_param_min, env_param_max, policy_param_min, policy_param_max, linspace_env, linspace_policy, param_space_size, state_space_size, env_param_space_size)
+    i_learning_rate = 0
+
+    stats = {}
+    for estimator in estimators:
+        stats[estimator] = []
+
+    for estimator in estimators:
+
+        print(estimator)
+        if estimator in ["GPOMDP", "REINFORCE", "REINFORCE-BASELINE"]:
+            off_policy = 0
+            simulation_param.batch_size = 10
+        else:
+            off_policy = 1
+            simulation_param.batch_size = 5
+
+        source_dataset = sc.SourceDataset(source_task, source_param, episodes_per_configuration, next_states_unclipped, actions_clipped, next_states_unclipped_denoised, n_config_cv)
+        simulation_param.learning_rate = learning_rates[i_learning_rate]
+
+        result = la.learnPolicy(env_param, simulation_param, source_dataset, estimator, off_policy=off_policy, model_estimation=0, multid_approx=0)
+
+        stats[estimator].append(result)
+
+        i_learning_rate += 1
+
+    return stats
 
 env_tgt = gym.make('LQG1D-v0')
 env_src = gym.make('LQG1D-v0')
@@ -20,24 +55,24 @@ env_param = sc.EnvParam(env_tgt, param_space_size, state_space_size, env_param_s
 mean_initial_param = -0.1 * np.ones(param_space_size)
 variance_initial_param = 0
 variance_action = 0.1
-batch_size = 4
+batch_size = 5
 num_batch = 350
 discount_factor = 0.99
-runs = 15
+runs = 20
 learning_rate = 1e-5
-ess_min = 50
+ess_min = 70
 adaptive = "No"
 
 simulation_param = sc.SimulationParam(mean_initial_param, variance_initial_param, variance_action, batch_size, num_batch, discount_factor, runs, learning_rate, ess_min, adaptive)
 
 # source task for lqg1d
-source_dataset_batch_size = 15
+source_dataset_batch_size = 20
 discount_factor = 0.99
-env_param_min = 0.6
+env_param_min = 0.5
 env_param_max = 1.5
 policy_param_min = -1
 policy_param_max = -0.1
-linspace_env = 10
+linspace_env = 11
 linspace_policy = 10
 n_config_cv = linspace_policy * linspace_env #number of configurations to use to fit the control variates
 
@@ -51,65 +86,15 @@ for i in range(omega_env.shape[0]):
     env_proposal.setParams(np.concatenate(([omega_env[i]], np.ravel(env_proposal.B), [env_proposal.sigma_noise**2])))
     model_proposals.append(env_proposal)
 
-estimators = ["MIS", "MIS-CV-BASELINE", "PD-MIS", "PD-MIS-CV-BASELINE", "GPOMDP"]
 
-#learning_rates = [2e-5, 6e-6, 1e-5, 2e-5, 1e-5, 1e-6, 1e-5, 1e-5, 1e-6, 1e-6, 1e-5]
-learning_rates = [6e-6, 6e-6, 1e-5, 2e-5, 7e-6]
-disc_rewards = {}
-policy = {}
-gradient = {}
-ess = {}
-n_def = {}
-for estimator in estimators:
-    disc_rewards[estimator] = []
-    policy[estimator] = []
-    gradient[estimator] = []
-    ess[estimator] = []
-    n_def[estimator] = []
+estimators = ["IS", "PD-IS", "MIS", "MIS-CV-BASELINE", "PD-MIS", "PD-MIS-CV-BASELINE", "GPOMDP"]
 
-for i_run in range(runs):
+learning_rates = [1e-6, 2e-6, 5e-6, 7e-6, 7e-6, 1e-5, 1e-5]#, 7e-6, 7e-6, 9e-6, 5e-6]
 
-    [source_task, source_param, episodes_per_configuration, next_states_unclipped, actions_clipped, next_states_unclipped_denoised] = stc.sourceTaskCreationAllCombinations(env_src, episode_length, source_dataset_batch_size, discount_factor, variance_action, env_param_min, env_param_max, policy_param_min, policy_param_max, linspace_env, linspace_policy, param_space_size, state_space_size, env_param_space_size)
-    source_dataset = sc.SourceDataset(source_task, source_param, episodes_per_configuration, next_states_unclipped, actions_clipped, next_states_unclipped_denoised, n_config_cv)
+seeds = [np.random.randint(1000000) for _ in range(runs)]
 
-    print("Run: " + str(i_run))
-    initial_param = np.random.normal(simulation_param.mean_initial_param, simulation_param.variance_initial_param)
+results = Parallel(n_jobs=10)(delayed(simulationParallel)(env_src, episode_length, source_dataset_batch_size, discount_factor, variance_action, env_param_min, env_param_max, policy_param_min, policy_param_max, linspace_env, linspace_policy, param_space_size, state_space_size, env_param_space_size, estimators, learning_rates, env_param, simulation_param, seed) for seed in seeds) #lqg1d
+#results = [simulationParallel(env_src, episode_length, source_dataset_batch_size, discount_factor, variance_action, env_param_min, env_param_max, policy_param_min, policy_param_max, linspace_env, linspace_policy, param_space_size, state_space_size, env_param_space_size, estimators, learning_rates, env_param, simulation_param, seed) for seed in seeds]
 
-    i_learning_rate = 0
-
-    for estimator in estimators:
-
-        print(estimator)
-        if estimator in ["GPOMDP", "REINFORCE", "REINFORCE-BASELINE"]:
-            off_policy = 0
-            simulation_param.batch_size = 20
-        else:
-            off_policy = 1
-
-        source_dataset = sc.SourceDataset(source_task, source_param, episodes_per_configuration, next_states_unclipped, actions_clipped, next_states_unclipped_denoised, n_config_cv)
-        simulation_param.learning_rate = learning_rates[i_learning_rate]
-
-        result = la.learnPolicyWithModelEstimation(env_param, simulation_param, source_dataset, model_proposals, estimator, off_policy=off_policy, model_estimation=0, dicrete_estimation=1, multid_approx=0) #1e-6
-
-        disc_rewards[estimator].append(result.disc_rewards)
-        policy[estimator].append(result.policy_parameter)
-        gradient[estimator].append(result.gradient)
-        ess[estimator].append(result.ess)
-        n_def[estimator].append(result.n_def)
-
-        i_learning_rate += 1
-
-with open('rewards.pkl', 'wb') as output:
-    pickle.dump(disc_rewards, output, pickle.HIGHEST_PROTOCOL)
-
-with open('policy.pkl', 'wb') as output:
-    pickle.dump(policy, output, pickle.HIGHEST_PROTOCOL)
-
-with open('gradient.pkl', 'wb') as output:
-    pickle.dump(gradient, output, pickle.HIGHEST_PROTOCOL)
-
-with open('ess.pkl', 'wb') as output:
-    pickle.dump(ess, output, pickle.HIGHEST_PROTOCOL)
-
-with open('n_def.pkl', 'wb') as output:
-    pickle.dump(n_def, output, pickle.HIGHEST_PROTOCOL)
+with open('results.pkl', 'wb') as output:
+    pickle.dump(results, output, pickle.HIGHEST_PROTOCOL)
