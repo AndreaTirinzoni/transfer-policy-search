4fca5a829811172e16e7c48270385c8f32808ed4
AndreaTirinzoni <andrea.tirinzoni@polimi.it>
2019-01-16 00:17:25 +0100

    Modified cartpole script

--- simulation_cartpole.py
+++ simulation_cartpole.py
@@ -20,41 +20,83 @@ def main():
 
     env_param = sc.EnvParam(env_tgt, param_space_size, state_space_size, env_param_space_size, episode_length)
 
-    mean_initial_param = np.random.normal(np.zeros(param_space_size), 0.1)
+    mean_initial_param = np.random.normal(np.zeros(param_space_size), 0.01)
     variance_initial_param = 0
     variance_action = 0.1
-    batch_size = 10
+    batch_size = 5
     discount_factor = 0.99
-    ess_min = 20
+    ess_min = 10
     adaptive = "Yes"
-    n_min = 10
+    n_min = 1
 
     simulation_param = sc.SimulationParam(mean_initial_param, variance_initial_param, variance_action, batch_size,
                                           num_batch, discount_factor, None, None, ess_min, adaptive, n_min)
 
+    policy_params = np.array([[-0.04058811, 0.06820783, 0.09962419, -0.01481458],
+                              [-0.04327763, 0.01926409, 0.10651812, 0.07304843],
+                              [-0.04660533, -0.08301117, 0.14598312, 0.31524803],
+                              [-0.04488895, -0.04959011, 0.20856307, 0.52564195],
+                              [-0.02085553, 0.11530108, 0.24525215, 0.58338479],
+                              [-0.03072567, 0.15546779, 0.27241488, 0.65833969],
+                              [-0.05493752, 0.11100809, 0.30213226, 0.73134919],
+                              [-0.02389198, 0.18004238, 0.30697023, 0.72447482],
+                              [-0.0702051, 0.17653729, 0.32254312, 0.72004621],
+                              [-0.09675066, 0.16063462, 0.32343255, 0.73801456],
 
-    # source task for cartpole
-    #policy_params = np.array([[-0.0259, 0.2541, 0.2797, 0.7054], [-0.0659, 0.0610, 0.0910, 0.3631]])#[-0.049, 0.176, 0.447, 0.810], [-0.105, -0.16, -0.0103, 0.128], [-0.131, 0.246, 0.402, 0.854], [-0.103, 0.158, -0.023, 0.124], [-0.039, 0.299, 0.386, 0.782], [-0.103, -0.137, -0.038, 0.12], [-0.111, -0.148, -0.027, 0.086], [-0.0115, 0.219, 0.416, 0.792], [-0.049, 0.176, 0.447, 0.810], [-0.105, -0.16, -0.0103, 0.128]])
-    policy_params = np.array([[-0.15719037, -0.12614755,  0.15791737, 0.11699056],
-                              [-0.0781, 0.1737, 0.2883, 0.6518],
-                              [-0.0757, -0.0379, 0.0585, 0.3026],
-                              [-0.15719037, -0.12614755, 0.15791737, 0.11699056],
-                              [-0.0554, 0.1725, 0.2940, 0.6916],
-                              [-0.0796, -0.0363, 0.0526, 0.3152],
-                              [-0.15719037, -0.12614755, 0.15791737, 0.11699056],
-                              [-0.103, 0.158, -0.023, 0.124],
-                              [-0.039, 0.299, 0.386, 0.782]])#, [-0.103, -0.137, -0.038, 0.12], [-0.111, -0.148, -0.027, 0.086]])#, , [-0.0115, 0.219, 0.416, 0.792], [-0.049, 0.176, 0.447, 0.810], [-0.105, -0.16, -0.0103, 0.128]])
-    env_params = np.array([[1.1, 0.6, 0.09],
-                           [1.1, 0.6, 0.09],
-                           [1.1, 0.6, 0.09],
-                           [1.5, 0.5, 0.09],
-                           [1.5, 0.5, 0.09],
-                           [1.5, 0.5, 0.09],
-                           [0.8, 0.8, 0.09],
-                           [0.8, 0.8, 0.09],
-                           [0.8, 0.8, 0.09]])#, [1.5, 0.5, 0.09], [1.5, 0.5, 0.09]])#, , [1.2, 0.9, 0.09], [1.2, 0.9, 0.09], ])
+                              [-0.04058811, 0.06820783, 0.09962419, -0.01481458],
+                              [-0.04327763, 0.01926409, 0.10651812, 0.07304843],
+                              [-0.04660533, -0.08301117, 0.14598312, 0.31524803],
+                              [-0.04488895, -0.04959011, 0.20856307, 0.52564195],
+                              [-0.02085553, 0.11530108, 0.24525215, 0.58338479],
+                              [-0.03072567, 0.15546779, 0.27241488, 0.65833969],
+                              [-0.05493752, 0.11100809, 0.30213226, 0.73134919],
+                              [-0.02389198, 0.18004238, 0.30697023, 0.72447482],
+                              [-0.0702051, 0.17653729, 0.32254312, 0.72004621],
+                              [-0.09675066, 0.16063462, 0.32343255, 0.73801456],
 
-    source_dataset_batch_size = 20
+                              [-0.04058811, 0.06820783, 0.09962419, -0.01481458],
+                              [-0.04327763, 0.01926409, 0.10651812, 0.07304843],
+                              [-0.04660533, -0.08301117, 0.14598312, 0.31524803],
+                              [-0.04488895, -0.04959011, 0.20856307, 0.52564195],
+                              [-0.02085553, 0.11530108, 0.24525215, 0.58338479],
+                              [-0.03072567, 0.15546779, 0.27241488, 0.65833969],
+                              [-0.05493752, 0.11100809, 0.30213226, 0.73134919],
+                              [-0.02389198, 0.18004238, 0.30697023, 0.72447482],
+                              [-0.0702051, 0.17653729, 0.32254312, 0.72004621],
+                              [-0.09675066, 0.16063462, 0.32343255, 0.73801456]])
+
+    env_params = np.array([[1.3, 0.4, 0.09],
+                           [1.3, 0.4, 0.09],
+                           [1.3, 0.4, 0.09],
+                           [1.3, 0.4, 0.09],
+                           [1.3, 0.4, 0.09],
+                           [1.3, 0.4, 0.09],
+                           [1.3, 0.4, 0.09],
+                           [1.3, 0.4, 0.09],
+                           [1.3, 0.4, 0.09],
+                           [1.3, 0.4, 0.09],
+                           [0.9, 0.6, 0.09],
+                           [0.9, 0.6, 0.09],
+                           [0.9, 0.6, 0.09],
+                           [0.9, 0.6, 0.09],
+                           [0.9, 0.6, 0.09],
+                           [0.9, 0.6, 0.09],
+                           [0.9, 0.6, 0.09],
+                           [0.9, 0.6, 0.09],
+                           [0.9, 0.6, 0.09],
+                           [0.9, 0.6, 0.09],
+                           [1.5, 0.7, 0.09],
+                           [1.5, 0.7, 0.09],
+                           [1.5, 0.7, 0.09],
+                           [1.5, 0.7, 0.09],
+                           [1.5, 0.7, 0.09],
+                           [1.5, 0.7, 0.09],
+                           [1.5, 0.7, 0.09],
+                           [1.5, 0.7, 0.09],
+                           [1.5, 0.7, 0.09],
+                           [1.5, 0.7, 0.09]])
+
+    source_dataset_batch_size = 10
     n_config_cv = policy_params.shape[0]
 
     [source_task, source_param, episodes_per_configuration, next_states_unclipped, actions_clipped,
@@ -72,31 +114,30 @@ def main():
 
         if estimator in ["GPOMDP", "REINFORCE", "REINFORCE-BASELINE"]:
             off_policy = 0
-            name = estimator
-            simulation_param.batch_size = 15
         else:
             off_policy = 1
-            simulation_param.batch_size = 10
 
         simulation_param.learning_rate = learning_rate
         if estimator.endswith("SR"): #if sample reuse
             source_dataset_batch_size = 1
-            simulation_param.batch_size = 5
-            discount_factor = 0.99
             policy_params = np.array([[0, 0, 0, 0]])
             env_params = np.array([[1.0, 0.5, 0.09]])
-            n_config_cv = 1
             name = estimator[:-3]
             [source_task, source_param, episodes_per_configuration, next_states_unclipped, actions_clipped,
              next_states_unclipped_denoised] = stc.sourceTaskCreationSpec(env_src, episode_length, source_dataset_batch_size,
                                                                           discount_factor, variance_action, policy_params,
                                                                           env_params, param_space_size, state_space_size,
                                                                           env_param_space_size)
+
+            source_dataset = sc.SourceDataset(source_task, source_param, episodes_per_configuration,
+                                              next_states_unclipped,
+                                              actions_clipped, next_states_unclipped_denoised, 1)
         else:
             name = estimator
 
-        source_dataset = sc.SourceDataset(source_task, source_param, episodes_per_configuration, next_states_unclipped,
-                                          actions_clipped, next_states_unclipped_denoised, n_config_cv)
+            source_dataset = sc.SourceDataset(source_task, source_param, episodes_per_configuration,
+                                              next_states_unclipped,
+                                              actions_clipped, next_states_unclipped_denoised, n_config_cv)
 
         simulation_param.learning_rate = learning_rate
 
@@ -126,15 +167,14 @@ def run(id, seed):
 
 
 # Number of jobs
-n_jobs = 10
+n_jobs = 20
 
 # Number of runs
 n_runs = 20
 
-estimators = ["PD-IS", "PD-MIS-CV-BASELINE", "PD-MIS-CV-BASELINE-SR", "GPOMDP"]
-#estimators = ["PD-MIS-SR", "PD-MIS-CV-BASELINE-SR", "GPOMDP"]
+estimators = ["GPOMDP", "PD-IS", "PD-MIS-CV-BASELINE", "PD-MIS-CV-BASELINE-SR"]
 learning_rates = [1e-3, 1e-3, 1e-3, 1e-3]
-num_batch = 70
+num_batch = 50
 
 # Base folder where to log
 folder = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
@@ -150,6 +190,8 @@ else:
 
 with open('results.pkl', 'wb') as output:
     pickle.dump(results, output, pickle.HIGHEST_PROTOCOL)
+
+print(folder)
 
 ################################################
 
