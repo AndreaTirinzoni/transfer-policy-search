1dd36448aaff770b486f77d2d3eda3d5bc7333e9
Marcello Restelli <marcello.restelli@gmail.com>
2019-01-17 22:18:10 +0100

    Changes to add the method getDensity

--- envs/minigolf.py
+++ envs/minigolf.py
@@ -6,6 +6,7 @@ from gym import spaces
 from gym.utils import seeding
 import numpy as np
 import math as m
+from scipy.stats import norm
 
 """
 Minigolf task.
@@ -36,6 +37,8 @@ class MiniGolf(gym.Env):
         self.sigma_noise = 0.3
         self.ball_radius = 0.02135
 
+        self.deceleration = 5 / 7 * self.friction * 9.81
+
         # gym attributes
         self.viewer = None
         low = np.array([self.min_pos])
@@ -56,25 +59,26 @@ class MiniGolf(gym.Env):
         self.sigma_noise = m.sqrt(env_param[-1])
 
     def step(self, action, render=False):
-        noise = self.np_random.randn() * self.sigma_noise
+        noise = 10
+        while abs(noise) > 1:
+            noise = self.np_random.randn() * self.sigma_noise
         u = np.clip(action*self.putter_length*(1 + noise), self.min_action, self.max_action)
 
         v_min = np.sqrt(10 / 7 * self.friction * 9.81 * self.state)
         v_max = np.sqrt( (2*self.hole_size - self.ball_radius)**2*(9.81/(2*self.ball_radius)) + v_min**2)
 
+        t = u / self.deceleration
+        xn = self.state - u * t + 0.5 * self.deceleration * t ** 2
+
         reward = 0
         done = True
-        xn = 0
         if u < v_min:
-            deceleration = 5/7*self.friction*9.81
-            t = u / deceleration
-            xn = self.state - u * t + 0.5 * deceleration * t ** 2
             reward = -1
             done = False
         elif u > v_max:
             reward = -100
 
-        xn = np.clip(xn, self.min_pos, self.max_pos)
+        #xn = np.clip(xn, self.min_pos, self.max_pos)
 
         self.state = xn
 
@@ -108,4 +112,12 @@ class MiniGolf(gym.Env):
         self.np_random, seed = seeding.np_random(seed)
         return [seed]
 
+    def getDensity(self, env_parameters, state, action, next_state):
+        putter_length = env_parameters[0]
+        friction = env_parameters[1]
+        sigma_noise = env_parameters[-1]
+        deceleration = 5 / 7 * friction * 9.81
+        u = np.sqrt(2 * deceleration * (state - next_state))
+        noise = (u / (action*putter_length) - 1) / sigma_noise
+        return norm.pdf(noise)
 
